a122d6c4d52b48306ff53462fe8feb897a9623c6
Gunwoo <74530907+pgw6541@users.noreply.github.com>
2023-10-22 15:08:17 +0900

    [level unrated] Title: 배열 두배 만들기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/unrated/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -1,9 +1,3 @@
-function solution(numbers) {
-    let arr = [] 
-    numbers.map( a => {
-        
-        arr.push(a*2)
-        
-    })
-    return arr
+const solution = (numbers) => {
+    return numbers.map( a => a*2)
 }
