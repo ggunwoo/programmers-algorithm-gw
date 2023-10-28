fed8b578a3cd98e51fd2d6ce035fa2967e39ea44
Gunwoo <74530907+pgw6541@users.noreply.github.com>
2023-10-28 22:42:31 +0900

    [level unrated] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,9 @@
+function solution(numbers, n) {
+    let sum = 0;
+    for(let i=0; i<=numbers.length; i++){
+        if(sum<=n){
+            sum += numbers[i]
+        } else { break; }
+    }
+    return sum
+}
