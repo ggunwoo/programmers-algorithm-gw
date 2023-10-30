231ffa4fe099a1f137bca9dc78bf19746c05ddd1
Gunwoo <74530907+pgw6541@users.noreply.github.com>
2023-10-31 03:26:23 +0900

    [level unrated] Title: 홀짝 구분하기, Time: 44.93 ms, Memory: 32.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+    if(line%2===0){
+        console.log(input + ' is even')
+    } else {
+        console.log(input + ' is odd')
+    
+    }
+        
+    
+    rl.close();
+})
