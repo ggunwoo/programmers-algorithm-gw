9a8d2927681157786d4c1c533f1c016e644e988e
Gunwoo <74530907+pgw6541@users.noreply.github.com>
2023-11-26 23:29:51 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.19 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,6 +1,6 @@
 function solution(date1, date2) {
-    const DateA = new Date(...date1)
-    const DateB = new Date(...date2)
+    const DateA = new Date(date1)
+    const DateB = new Date(date2)
     
     return DateA < DateB ? 1 : 0
 }
