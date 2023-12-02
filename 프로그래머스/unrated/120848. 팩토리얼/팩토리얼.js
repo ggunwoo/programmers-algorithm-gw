d136e9f3adde196d4fc6d547e0d34a50348db801
Gunwoo <74530907+pgw6541@users.noreply.github.com>
2023-12-02 19:03:20 +0900

    [level unrated] Title: 팩토리얼, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.js
@@ -1,9 +1,9 @@
 function solution(n) {
-    const sum =(i)=> Array(i).fill(1).map((a,b) => a+b).reduce((a,b)=> a*b);
-    let j = 1;
+    const sum =(i)=> Array(i+1).fill(1).map((a,b) => a+b).reduce((a,b)=> a*b);
+    let j = 0;
     while(true){
         if(n<sum(j)) break;
         j++
     }
-    return j-1
+    return j
 }
