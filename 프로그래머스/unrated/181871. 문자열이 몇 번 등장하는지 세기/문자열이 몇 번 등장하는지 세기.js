7e4b163a3c3aa4517b195b5847f72358203446e4
Gunwoo <74530907+pgw6541@users.noreply.github.com>
2023-12-05 21:12:45 +0900

    [level unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -1,7 +1,4 @@
 function solution(myString, pat) {
     var answer = 0;
-    for(let i=0; i<myString.length; i++){
-        myString.slice(i, (i+pat.length))===pat ? answer++ : null
-    }
     return answer;
 }
