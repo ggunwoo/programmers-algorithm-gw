c6457d30cdfe6408cb9a8cabc366af3e6c05d4f0
Gunwoo <74530907+pgw6541@users.noreply.github.com>
2023-12-18 18:04:01 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,14 @@
+function solution(num) {
+    var answer = 0;
+    for(let i=0; i<500; i++){
+        if(num===1) break;
+        if(num%2===0){
+            num=num/2
+            answer++
+        } else {
+            num=num*3+1
+            answer++
+        }
+    }
+    return answer>=500 ? -1 : answer
+}
